0287cbfdd7a752b1b049837352eeecfd500d6fa5
mburman <mburman@andrew.cmu.edu>
2014-04-16 17:27:10 -0400

    Added command line argument parsing.

--- src/github.com/mburman/hooli/hooli.go
+++ src/github.com/mburman/hooli/hooli.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"strings"
+)
+
+const defaultPort = 9009
+
+var (
+	nodePort = flag.Int("port", defaultPort, "port number for accepter to listen on")
+	nodeID   = flag.Uint("id", 0, "unique id to be used as lower order bits of the proposal number")
+)
+
+type ports []string
+
+var acceptorPorts ports
+
+// Set is the method to set the flag value, part of the flag.Value interface.
+// Set's argument is a string to be parsed to set the flag.
+// It's a comma-separated list, so we split it.
+func (p *ports) Set(value string) error {
+	if len(*p) > 0 {
+		return errors.New("interval flag already set")
+	}
+	for _, port := range strings.Split(value, ",") {
+		*p = append(*p, port)
+	}
+	return nil
+}
+
+func (p *ports) String() string {
+	return fmt.Sprint(*p)
+}
+
+func init() {
+	flag.Var(&acceptorPorts, "ports", "comma-separated list of accepter ports")
+}
+
+func printFlags() {
+	fmt.Println("Arguments...")
+	fmt.Println("nodePort: ", *nodePort)
+	for _, port := range acceptorPorts {
+		fmt.Println("Acceptor port: ", port)
+	}
+	fmt.Println("nodeID: ", *nodeID)
+}
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	flag.Parse()
+	printFlags()
 }
