3d3a068f203227dea31fe9dcda1051c70bd25457
mburman <mburman@andrew.cmu.edu>
2014-04-22 00:50:03 -0400

    Acceptor code done. Not tested.

--- src/github.com/mburman/hooli/acceptor/acceptor.go
+++ src/github.com/mburman/hooli/acceptor/acceptor.go
@@ -3,6 +3,7 @@ package acceptor
 import (
 	"fmt"
 	"github.com/mburman/hooli/rpc/acceptorrpc"
+	"github.com/mburman/hooli/rpc/proposerrpc"
 	"io/ioutil"
 	"log"
 	"net"
@@ -15,24 +16,56 @@ var LOGE = log.New(os.Stderr, "ERROR ", log.Lmicroseconds|log.Lshortfile)
 var LOGV = log.New(ioutil.Discard, "VERBOSE ", log.Lmicroseconds|log.Lshortfile)
 
 type acceptorObj struct {
+	minProposal     *acceptorrpc.Proposal
+	acceptedMessage *proposerrpc.Message
 }
 
 // port: port to start the acceptorObj on.
 func NewAcceptor(port int) *acceptorObj {
 	var a acceptorObj
+	a.minProposal = &acceptorrpc.Proposal{Number: -1, ID: -1}
+	a.acceptedMessage = nil
+
 	setupRPC(&a, port)
 	return &a
 }
 
 func (a *acceptorObj) Prepare(args *acceptorrpc.PrepareArgs, reply *acceptorrpc.PrepareReply) error {
+	if args.Proposal.Number < a.minProposal.Number {
+		reply.Status = acceptorrpc.CANCEL
+	} else if args.Proposal.Number == a.minProposal.Number && args.Proposal.ID < a.minProposal.ID {
+		reply.Status = acceptorrpc.CANCEL
+	} else {
+		if a.minProposal.Number != -1 {
+			// Something was previously accepted.
+			reply.Status = acceptorrpc.PREV_ACCEPTED
+		} else {
+			reply.Status = acceptorrpc.OK
+		}
+	}
+
+	reply.AcceptedProposalNumber = a.minProposal.Number
+	reply.AcceptedMessage = *a.acceptedMessage
 	return nil
 }
 
 func (a *acceptorObj) Accept(args *acceptorrpc.AcceptArgs, reply *acceptorrpc.AcceptReply) error {
+	if args.Proposal.Number < a.minProposal.Number {
+		reply.Status = acceptorrpc.CANCEL
+	} else if args.Proposal.Number == a.minProposal.Number && args.Proposal.ID < a.minProposal.ID {
+		reply.Status = acceptorrpc.CANCEL
+	} else {
+		reply.Status = acceptorrpc.OK
+		a.acceptedMessage = &args.ProposalMessage
+		a.minProposal = &args.Proposal
+	}
+
+	reply.MinProposalNumber = a.minProposal.Number
 	return nil
 }
 
 func (a *acceptorObj) Commit(args *acceptorrpc.CommitArgs, reply *acceptorrpc.CommitReply) error {
+	fmt.Println("Committed message:", args.Message)
 	return nil
 }
 
