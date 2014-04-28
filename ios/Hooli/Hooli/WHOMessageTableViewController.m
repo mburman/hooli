ee72569af7b4bc0e04687114f1a5292b1edab9e3
Dylan Koenig <koenigup@gmail.com>
2014-04-27 22:04:54 -0400

    UIing the shit out of it

--- ios/Hooli/Hooli/WHOMessageTableViewController.m
+++ ios/Hooli/Hooli/WHOMessageTableViewController.m
@@ -25,11 +25,15 @@ double kMessageRadius = 1.0;
     self = [super initWithStyle:style];
     if (self) {
         // Custom initialization
+        UIColor* hooliColor =[UIColor colorWithRed:70.0/255 green:235.0/255 blue:150.0/255 alpha:1.0];
+        UIColor* brownColor = [UIColor colorWithRed:78.0/255 green:46.0/255 blue:40.0/255 alpha:1.0];
         [self.tableView setSeparatorInset:UIEdgeInsetsZero];
+        [self.tableView setSeparatorColor:hooliColor];
+        [self.tableView setBackgroundColor:brownColor];
         UILabel* titleLabel = [[UILabel alloc] init];
         [titleLabel setText:@"Hooli"];
         [titleLabel setFont:[UIFont fontWithName:@"Superclarendon-BlackItalic" size:25.0]];
-        [titleLabel setTextColor:[UIColor colorWithRed:70.0/255 green:235.0/255 blue:150.0/255 alpha:.85]];
+        [titleLabel setTextColor:hooliColor];
 //        [titleLabel setAlpha:0.75];
         [titleLabel.layer setShadowColor:[UIColor darkGrayColor].CGColor];
         [titleLabel.layer setShadowOffset:(CGSize) { .width = 1.5, .height = 1.5 }];
@@ -58,7 +62,9 @@ double kMessageRadius = 1.0;
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
     
-    UIBarButtonItem* newMessageButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(newMessage:)];
+//    UIColor* hooliColor =[UIColor colorWithRed:70.0/255 green:235.0/255 blue:150.0/255 alpha:.85];
+    UIBarButtonItem* newMessageButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCompose target:self action:@selector(newMessage:)];
+    [newMessageButton setTintColor:[UIColor brownColor]];
     self.navigationItem.rightBarButtonItem = newMessageButton;
     
     [self.tableView setRowHeight:150.0];
@@ -123,6 +129,7 @@ double kMessageRadius = 1.0;
         [self.tableView reloadData];
     } failure:^(NSURLSessionDataTask *task, NSError *error) {
         NSLog(@"REST GET failure wih error: %@", error);
+        [self.refreshControl endRefreshing];
     }];
 }
 
--- ios/Hooli/Hooli/WHONewMessageViewController.m
+++ ios/Hooli/Hooli/WHONewMessageViewController.m
@@ -27,18 +27,48 @@
 {
     [super viewDidLoad];
     
-    UIBarButtonItem* submitButton = [[UIBarButtonItem alloc] initWithTitle:@"Done" style:UIBarButtonItemStylePlain target:self action:@selector(submit:)];
-    self.navigationItem.rightBarButtonItem = submitButton;
+    UIColor* hooliColor =[UIColor colorWithRed:70.0/255 green:235.0/255 blue:150.0/255 alpha:1.0];
+    UIColor* brownColor = [UIColor colorWithRed:78.0/255 green:46.0/255 blue:40.0/255 alpha:1.0];
+    
     self.messageField.delegate = self;
+    
+    [self.navigationItem.leftBarButtonItem setTintColor:brownColor];
+    
+    UILabel* titleLabel = [[UILabel alloc] init];
+    [titleLabel setText:@"Hooli"];
+    [titleLabel setFont:[UIFont fontWithName:@"Superclarendon-BlackItalic" size:25.0]];
+    [titleLabel setTextColor:hooliColor];
+//        [titleLabel setAlpha:0.75];
+    [titleLabel.layer setShadowColor:[UIColor darkGrayColor].CGColor];
+    [titleLabel.layer setShadowOffset:(CGSize) { .width = 1.5, .height = 1.5 }];
+    [titleLabel.layer setShadowRadius:1.5];
+    [titleLabel.layer setShadowOpacity:.65];
+    [titleLabel sizeToFit];
+    [self.navigationItem setTitleView:titleLabel];
+    
+    [self.view setBackgroundColor:brownColor];
+    [self.messageField setBackgroundColor:brownColor];
+    [self.messageField setTextColor:hooliColor];
+//    [self.messageField setTextContainerInset:UIEdgeInsetsMake(172.0, self.messageField.window.bounds.origin.x, self.messageField.window.bounds.origin.y+self.messageField.window.bounds.size.height, self.messageField.window.bounds.origin.x+self.messageField.window.bounds.size.width)];
     
     //for Done button on keyboard
 //    [self.messageField addTarget:self action:@selector(messageView:) forControlEvents:UIControlEventEditingDidEndOnExit];
 }
 
 - (void)submit:(id)sender {
-    //TODO compute user location
     [self.delegate receivedNewMessage:self.messageField.text];
     [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void)showPostButton {
+    UIColor* brownColor = [UIColor colorWithRed:78.0/255 green:46.0/255 blue:40.0/255 alpha:.85];
+    UIBarButtonItem* submitButton = [[UIBarButtonItem alloc] initWithTitle:@"Post" style:UIBarButtonItemStylePlain target:self action:@selector(submit:)];
+    [submitButton setTintColor:brownColor];
+    [self.navigationItem setRightBarButtonItem:submitButton animated:YES];
+}
+
+- (void)hidePostButton {
+    [self.navigationItem setRightBarButtonItem:nil animated:YES];
 }
 
 - (void)didReceiveMemoryWarning
@@ -52,14 +82,32 @@
 }
 
 -(void)textViewDidBeginEditing:(UITextView *)textView {
+    [UIView transitionWithView:self.placeholderLabel
+                      duration:.3
+                       options:UIViewAnimationOptionTransitionCrossDissolve
+                    animations:NULL
+                    completion:NULL];
     self.placeholderLabel.hidden = YES;
 }
 
-//-(void)textViewDidChange:(UITextView *)textView {
+-(void)textViewDidChange:(UITextView *)textView {
 //    self.placeholderLabel.hidden = [self.messageField.text length] > 0;
-//}
+    if ([self.messageField.text length] > 0) {
+        if (self.navigationItem.rightBarButtonItem == nil) {
+            [self showPostButton];
+        }
+    }
+    else if (self.navigationItem.rightBarButtonItem != nil) {
+        [self hidePostButton];
+    }
+}
 
 -(void)textViewDidEndEditing:(UITextView *)textView {
+    [UIView transitionWithView:self.placeholderLabel
+                      duration:.3
+                       options:UIViewAnimationOptionTransitionCrossDissolve
+                    animations:NULL
+                    completion:NULL];
     self.placeholderLabel.hidden = [self.messageField.text length] > 0;
 }
 
